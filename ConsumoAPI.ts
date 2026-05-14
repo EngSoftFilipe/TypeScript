b4c2f7dbb237322c5471e6db9ac0634f0f4aecdc
EngSoftFilipe <filipegabriel123git@gmail.com>
2026-05-13 22:22:42 -0300

    Primeiro consumo de API em TS

---
+++ ConsumoAPI.ts
@@ -0,0 +1,18 @@
+interface Product{
+    title: string;
+    id: number;
+    price: number
+}
+
+async function getProducts(): Promise <void> {
+    try{
+        const answer = await fetch("https://fakestoreapi.com/products/1");
+        const product: Product = await answer.json();
+        console.log(`Nome: ${product.title}\nID: ${product.id}\nPreço: ${product.price}`);  
+    } 
+    
+    catch(error){
+        console.log("Erro na requisição!");
+    }
+}
+getProducts();
