57ba8f5d4a2b5b3a63250e8cdebf7dd361ecfc23
EngSoftFilipe <filipegabriel123git@gmail.com>
2026-05-15 22:09:43 -0300

    Correção no código

--- ConsumoAPI.ts
+++ ConsumoAPI.ts
@@ -8,9 +8,9 @@ async function getProducts(): Promise <void> {
     try{
         const answer = await fetch("https://fakestoreapi.com/products/1");
         const product: Product = await answer.json();
-        console.log(`Nome: ${product.title}\nID: ${product.id}\nPreço: ${product.price}`);  
+        console.log(`Name: ${product.title}\nID: ${product.id}\nPrice: ${product.price}`);  
     } catch(error){
-        console.log("Erro na requisição!");
+        console.log("Requisition error!");
     }
 }
 getProducts();
